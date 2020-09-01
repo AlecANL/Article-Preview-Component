493332df7f7048b8962e593f8651abf050b81343
alec <alexeialvarez4@gmail.com>
2020-08-31 19:31:43 -0700

    finished challenge

---
+++ app.js
@@ -0,0 +1,6 @@
+const sharedBtn = document.querySelector('.icon-shared');
+const modal = document.getElementById('modal');
+
+sharedBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
